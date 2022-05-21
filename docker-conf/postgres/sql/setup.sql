534e5d47f0bde07fb57b213a7d749605fe8f9edc
Jaydeep Parmar <ext_jparmar@falabella.cl>
2022-05-21 20:56:49 +0530

    Add docker-compose, DB setup files

---
+++ docker-conf/postgres/sql/setup.sql
@@ -0,0 +1,19 @@
+create TABLE IF NOT EXISTS accounts (
+    accountId INT GENERATED ALWAYS AS IDENTITY,
+    customerId VARCHAR(255) NOT NULL,
+    balanceList jsonb,
+    PRIMARY KEY(accountId)
+);
+
+create TABLE IF NOT EXISTS transactions (
+    transactionId INT GENERATED ALWAYS AS IDENTITY,
+    accountId INT,
+    direction VARCHAR(5),
+    amount INTEGER,
+    currency VARCHAR(5),
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY(transactionId),
+    CONSTRAINT fk_account
+        FOREIGN KEY(accountId)
+    	    REFERENCES accounts(accountId)
+);
